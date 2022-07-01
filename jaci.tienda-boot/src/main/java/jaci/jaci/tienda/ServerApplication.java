db4a36779516dcde6717a076e296d09cd86c6ece
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-07-01 11:30:51 +0200

    Changed member repository to CampusDual repository

---
+++ jaci.tienda-boot/src/main/java/jaci/jaci/tienda/ServerApplication.java
@@ -0,0 +1,13 @@
+package jaci.jaci.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
