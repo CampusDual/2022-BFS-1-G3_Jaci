cfd265f28f296f2bc2d55149c3368f59d79ef3d5
“carlosgb3000@gmail.com” <carlosgb3000@gmail.com>
2022-07-08 11:55:53 +0200

    primera historia completa

--- jaci.frontend/src/app/main/sales/sales-home/sales-home.component.ts
+++ jaci.frontend/src/app/main/sales/sales-home/sales-home.component.ts
@@ -1,44 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+
 import { FilterExpressionUtils, Expression } from 'ontimize-web-ngx';
 
 @Component({
-  selector: 'app-sales-home',
-  templateUrl: './sales-home.component.html',
-  styleUrls: ['./sales-home.component.css']
+   selector: 'app-sales-home',
+   templateUrl: './sales-home.component.html',
+   styleUrls: ['./sales-home.component.css']
 })
 export class SalesHomeComponent implements OnInit {
 
 
+   constructor() { }
 
-  constructor() { }
+   ngOnInit() {
+   }
 
-  ngOnInit() {
-  }
+   createFilter(values: Array<{ attr, value }>): Expression {
+      let filters: Array<Expression> = [];
+      values.forEach(fil => {
+         if (fil.value) {
+            if (fil.attr === 'STARTDATE_I') {
+               filters.push(FilterExpressionUtils.buildExpressionMoreEqual('DATE', fil.value));
+            }
+            if (fil.attr === 'STARTDATE_E') {
+               filters.push(FilterExpressionUtils.buildExpressionLessEqual('DATE', fil.value));
+            }
+         }
+      });
 
-  createFilter(values: Array<{ attr, value }>): Expression {
-    let filters: Array<Expression> = [];
-    values.forEach(fil => {
-       if (fil.value) {
-          if (fil.attr === 'STARTDATE_I') {
-             filters.push(FilterExpressionUtils.buildExpressionMoreEqual('STARTDATE', fil.value));
-          }
-          if (fil.attr === 'STARTDATE_E') {
-             filters.push(FilterExpressionUtils.buildExpressionLessEqual('STARTDATE', fil.value));
-          }
-          if (fil.attr === 'ENDDATE_I') {
-             filters.push(FilterExpressionUtils.buildExpressionMoreEqual('ENDDATE', fil.value));
-          }
-          if (fil.attr === 'ENDDATE_E') {
-             filters.push(FilterExpressionUtils.buildExpressionLessEqual('ENDDATE', fil.value));
-          }
-       }
-    });
-
-    if (filters.length > 0) {
-       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
-    } else {
-       return null;
-    }
- }
-
+      if (filters.length > 0) {
+         return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+      } else {
+         return null;
+      }
+   }
 }
+
