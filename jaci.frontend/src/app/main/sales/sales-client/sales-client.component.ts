6c9dce464c4c64e892c0cf1c3a3671157b4aa172
DESKTOP-7PGLGTD\chivu <fernandezmendez99@gmail.com>
2022-07-21 09:50:23 +0200

    product arreglado y client sales creado

---
+++ jaci.frontend/src/app/main/sales/sales-client/sales-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-client',
+  templateUrl: './sales-client.component.html',
+  styleUrls: ['./sales-client.component.css']
+})
+export class SalesClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- jaci.frontend/src/app/main/sales/sales.module.ts
+++ jaci.frontend/src/app/main/sales/sales.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { SalesRoutingModule } from './sales-routing.module';
 import { SalesHomeComponent } from './sales-home/sales-home.component';
+import { SalesClientComponent } from './sales-client/sales-client.component';
 
 
 @NgModule({
-  declarations: [SalesHomeComponent],
+  declarations: [SalesHomeComponent, SalesClientComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
