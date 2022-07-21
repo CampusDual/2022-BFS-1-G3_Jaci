d6afc3e1e8ec443581735bfa820d71cb5455d027
DESKTOP-7PGLGTD\chivu <fernandezmendez99@gmail.com>
2022-07-21 12:27:09 +0200

    client by sales

--- jaci.frontend/src/app/main/sales/sales-routing.module.ts
+++ jaci.frontend/src/app/main/sales/sales-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SalesClientComponent } from './sales-client/sales-client.component';
 import { SalesHomeComponent } from './sales-home/sales-home.component';
 
 
 const routes: Routes = [{
   path : '',
   component: SalesHomeComponent
-},
-{
-  path : 'client/:CLIENT_ID',
-  component: SalesClientComponent
 }];
 
 @NgModule({
