f95c6f10c0b69c1247e8dd2778e5e95b06926a7b
Ignaciorguez <ignaciorodriguezeyre@gmail.com>
2022-07-29 14:36:23 +0200

    Adding images and o-card-menu-layout for the home-page

--- jaci.frontend/src/app/shared/app.menu.config.ts
+++ jaci.frontend/src/app/shared/app.menu.config.ts
@@ -2,9 +2,44 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+  {
+    id: 'views', name: 'VIEW', icon: 'remove_red_eye', opened: true,
+    items: [
+      {
+        id: 'product',
+        name: 'Products',
+        route: '/main/products',
+        icon: 'inventory',
+        image: 'assets/images/Product.jpg',
+      },
+      {
+        id: 'sales',
+        name: 'Sales',
+        route: '/main/sales',
+        icon: 'sell',
+        image: 'assets/images/Sales.jpg',
+      },
+      {
+        id: 'clients',
+        name: 'Clients',
+        route: '/main/clients',
+        icon: 'group',
+        image: 'assets/images/Clients.jpg',
+      },
+      {
+        id: 'workers',
+        name: 'Workers',
+        route: '/main/workers',
+        icon: 'engineering',
+        image: 'assets/images/Workers.jpg',
+      }
+    ]
+  }
+  /*
   { id: 'product', name: 'Products', icon: 'inventory', route: '/main/products' },
   { id: 'sales', name: 'Sales', icon: 'sell', route: '/main/sales' },
   { id: 'clients', name: 'Clients', icon: 'group', route: '/main/clients' },
   { id: 'workers', name: 'Workers', icon: 'engineering', route: '/main/workers' },
+  */
   /*{ id: 'logout', name: 'LOGOUT', route: '/login', icon: 'close', confirm: 'yes' }*/
 ];
